1ddb9c6cf8eebabcb974ad831532523687016da6
amordz72 <amordz72@gmail.com>
2021-05-28 20:00:27 +0100

    DROP TABLE IF EXISTS

---
+++ create-table-template.sql
@@ -0,0 +1,18 @@
+
+
+
+
+DROP TABLE IF EXISTS prod;
+CREATE TABLE prod(  
+    prId int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+    prName varchar(255),
+    prPrixA DECIMAL(8,2) NOT NULL ,
+    prPrixS DECIMAL(8,2) NOT NULL ,
+    prQtyLast int  NOT NULL ,
+   caNum INT NOT NULL ,
+
+    create_time DATETIME COMMENT 'create time',
+    update_time DATETIME COMMENT 'update time',
+    FOREIGN KEY (caNum) REFERENCES category(caId)
+  
+) default charset utf8 comment '';
